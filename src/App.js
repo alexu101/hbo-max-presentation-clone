7a5be207b156c6e0703b8876735d74edcf893b7d
Tanase Alex <alextanase60@yahoo.com>
2022-03-04 04:16:19 +0200

    working on header and gradients

--- src/App.js
+++ src/App.js
@@ -2,55 +2,26 @@ import React from 'react';
 import logo from './logo.svg';
 import { Counter } from './features/counter/Counter';
 import './App.css';
+import image from "./pics/logo.png";
+import heroes from "./pics/heroes.png"
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <Counter />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <span>
-          <span>Learn </span>
-          <a
-            className="App-link"
-            href="https://reactjs.org/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            React
-          </a>
-          <span>, </span>
-          <a
-            className="App-link"
-            href="https://redux.js.org/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Redux
-          </a>
-          <span>, </span>
-          <a
-            className="App-link"
-            href="https://redux-toolkit.js.org/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Redux Toolkit
-          </a>
-          ,<span> and </span>
-          <a
-            className="App-link"
-            href="https://react-redux.js.org/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            React Redux
-          </a>
-        </span>
-      </header>
+      <div className="Section1">
+        <header>
+          <img src={image} />
+          <h4>INTRĂ ÎN CONT</h4>
+        </header>
+        <img src={heroes} alt="" />
+        <h1>HBO Max sosește pe 8 martie!</h1>
+        <h2>Pregatește-te de o experiență de vizionare diferită</h2>
+        <h4>INTRĂ ÎN CONT</h4>
+        <h4>INTRĂ ÎN CONT</h4>
+        <h4>INTRĂ ÎN CONT</h4>
+        <h4>INTRĂ ÎN CONT</h4>
+        <h4>INTRĂ ÎN CONT</h4>
+      </div>
     </div>
   );
 }
