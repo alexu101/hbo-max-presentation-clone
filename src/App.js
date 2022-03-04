dcafc2b5c5c8882a60ef6eefcf118079d810b50c
Tanase Alex <alextanase60@yahoo.com>
2022-03-04 16:09:56 +0200

    section 1 and section 2 done - visual

--- src/App.js
+++ src/App.js
@@ -21,11 +21,11 @@ function App() {
         <form>
           <input type="text" placeholder='Adresa de e-mail' />
           <buttOn>ȚINE-MĂ LA CURENT!</buttOn>
+          <div className="policy">
+            <p>Prin completarea adresei de e-mail, accepți să primești e-mail-uri promoționale și alte oferte de la HBO Max (o companie WarnerMedia) și {<a href='www.google.ro'>PARTENERII</a>}  săi prin e-mail, social media și alte canale. Pentru a-ți retrage consimțământul și pentru a afla detalii despre drepturile tale, vezi opțiunile disponibile în {<a href='www.google.ro'>POLITICA DE CONFIDENȚIALITATE</a>}.</p>
+          </div>
+
         </form>
-        <div className="policy">
-          <p>Prin completarea adresei de e-mail, accepți să primești e-mail-uri promoționale și alte oferte de la HBO Max (o companie WarnerMedia) și </p>
-          <p>PARTENERII săi prin e-mail, social media și alte canale. Pentru a-ți retrage consimțământul și pentru a afla detalii despre drepturile tale, vezi opțiunile disponibile în POLITICA DE CONFIDENȚIALITATE.</p>
-        </div>
       </div>
     </div>
   );
